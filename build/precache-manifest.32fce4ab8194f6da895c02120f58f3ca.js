116871ee442816c7e1c53d9dcfea0be58e47d976
Jason Sofokleous <jasonsofokleous@Jasons-Macbook-Pro.local>
2020-09-16 23:43:12 +0100

    initial commit

---
+++ build/precache-manifest.32fce4ab8194f6da895c02120f58f3ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "beaa9749e1cdfe305f0bcdd6337ccf51",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7a8cb2d1ef2ae84419a9",
+    "url": "/static/css/main.a8636a45.chunk.css"
+  },
+  {
+    "revision": "e87c645f4c4afa1d870f",
+    "url": "/static/js/2.74d830e1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.74d830e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a8cb2d1ef2ae84419a9",
+    "url": "/static/js/main.15c9f6cc.chunk.js"
+  },
+  {
+    "revision": "25705b9036579a848134",
+    "url": "/static/js/runtime-main.270072aa.js"
+  }
+]);
