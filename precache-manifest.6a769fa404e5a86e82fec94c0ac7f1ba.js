d712b124342ee1c7420dd26eb99b95168063db82
Jason Sofokleous <jasonsofokleous@Jasons-Macbook-Pro.local>
2020-09-17 01:04:56 +0100

    Updates

---
+++ precache-manifest.6a769fa404e5a86e82fec94c0ac7f1ba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ac0f682bc751dd14d4bd4d903887638",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c1643ad950ad1cc929fa",
+    "url": "/static/css/main.a8636a45.chunk.css"
+  },
+  {
+    "revision": "e87c645f4c4afa1d870f",
+    "url": "/static/js/2.74d830e1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.74d830e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1643ad950ad1cc929fa",
+    "url": "/static/js/main.eb23e3a6.chunk.js"
+  },
+  {
+    "revision": "25705b9036579a848134",
+    "url": "/static/js/runtime-main.270072aa.js"
+  }
+]);
